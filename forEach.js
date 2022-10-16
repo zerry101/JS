8742fbec42ecaeb15887e21fa53aa1c1e4920b28
zerry101 <macone394@gmail.com>
2022-10-16 21:44:56 +0530

    m

--- forEach.js
+++ forEach.js
@@ -2,3 +2,12 @@ const fruits=["apple","banana","mango"];
 for(let fruit of fruits){
     console.log(fruit);
 }
+
+console.log("<br>");
+const [myvar1,myvar2,myvar3]=fruits;
+console.log(myvar1);
+console.log(myvar2);
+console.log(myvar3);
+
+console.log("br");
+console.log(fruits.slice(2));
