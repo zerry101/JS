56fd5656bb5fe1ed669000e0da341322d1c7631a
zerry101 <macone394@gmail.com>
2022-11-20 18:29:23 +0530

    m

--- Spread_Operator.js
+++ Spread_Operator.js
@@ -1,2 +1,9 @@
 const array1= [1,2,3];
 const array2=[4,5,6];
+
+const arr=[...array1,array2];
+console.log(arr);
+// here array2 will be processed as an array element even if it is array
+// ... spread an element consisting multiple elements refer below example 
+const newarr=[..."abcdefgh"];
+console.log(newarr);
