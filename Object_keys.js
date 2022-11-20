06af794070a9bc74dfbae69cd5e8eabe5d0f5e4a
zerry101 <macone394@gmail.com>
2022-11-20 15:13:43 +0530

    m

--- Object_keys.js
+++ Object_keys.js
@@ -1,10 +1,16 @@
 const person = {
     name: "zishan",
     age: 22,
-    hobbies: ["guitar", "sleeping"],
+    hobbies: ["guitar", "sleeping"] 
   };
 
 //   console.log(Object.keys(person));
 console.log(person[Object.keys(person)[2]]);
 console.log(Object.values(person)[0]);
 console.log(Object.keys(person)[0]);
+
+
+for(let key of Object.keys(person))
+{
+    console.log(person[key]);
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,9 +1,5 @@
-const person = {
-    name: "zishan",
-    age: 22,
-    hobbies: ["guitar", "sleeping"],
-  };
 
-//   console.log(Object.keys(person));
-console.log(person[Object.keys(person)[2]]);
-console.log(Object.keys(person)[2-1]);
+// for(let key of Object.keys(person))
+// {
+//     console.log(person[key]);
+// }
