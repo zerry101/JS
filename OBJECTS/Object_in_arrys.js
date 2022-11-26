3b432b48cb5ee9a764a0a73fbe04616f8a0129f8
zerry101 <macone394@gmail.com>
2022-11-26 21:25:48 +0530

    m

--- OBJECTS/Object_in_arrys.js
+++ OBJECTS/Object_in_arrys.js
@@ -3,10 +3,8 @@ users=[
     {userId:2,firstName:'raaj',gender:'male'},
     {userId:3,firstName:'drek',gender:'male'}
 ]
-
-// const [user1,user2,user3]=users;
-// console.log(user1);
-
-const [{firstName},,{gender}]=users;
-console.log(firstName);
-console.log(gender);
+// console.log(use);
+for(let user of users)
+{
+    console.log(Object.values(user));
+}
--- OBJECTS/nested_destructuring.js
+++ OBJECTS/nested_destructuring.js
@@ -2,7 +2,7 @@ users=[
     {userId:1,firstName:'harshit',gender:'male'},
     {userId:2,firstName:'raaj',gender:'male'},
     {userId:3,firstName:'drek',gender:'male'}
-]
+];
 
 // const [user1,user2,user3]=users;
 // console.log(user1);
@@ -10,3 +10,12 @@ users=[
 const [{firstName},,{gender}]=users;
 console.log(firstName);
 console.log(gender);
+
+const obj={
+    key1:"4",
+    key2:"5"
+};
+
+const {key1,key2}=obj
+
+console.log(key1);
