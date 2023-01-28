703a1d1e2ca9fb0e11da235462d570d32b96e782
zerry101 <macone394@gmail.com>
2023-01-28 17:25:16 +0530

    m

---
+++ OOPS/Encapsulation/Encapsulation.js
@@ -0,0 +1,58 @@
+class BankAccount {
+
+    customerName;
+    accountNumber;
+    balance;
+
+    constructor(customerName, balance = 0) {
+        this.customerName = customerName;
+        this.balance = balance;
+        this.accountNumber = Date.now();
+
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+    }
+
+    withdraw(amount) {
+        this.balance -= amount;
+    }
+}
+
+// Inheritace below
+
+class CurrentAccount extends BankAccount{
+    transactionLimit;
+    takePersonalLoan;
+ constructor(customerName,balance=0,list)
+ {
+     super(customerName,balance) ;
+     // Inheritance in constructor above
+    this.transactionLimit=10000;
+    this.takePersonalLoan=(amount)=>{
+        console.log(`You will recieve of  ${amount}  of personal loan`);
+    }
+ }   
+ 
+}
+
+const rakeshAccount=new CurrentAccount('rk',3000);
+rakeshAccount.takePersonalLoan(30000);
+console.log(rakeshAccount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
