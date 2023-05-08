dd95c674052561a1928d8a0c31f7ee68afebfd07
zerry101 <macone394@gmail.com>
2023-05-08 22:41:20 +0530

    m

--- js_concepts/spread_operator.js
+++ js_concepts/spread_operator.js
@@ -1,20 +1,30 @@
-function sum(name,course,...args){
-    let sum=0;
-for(i in args){
-        sum+=args[i];
-        // console.log(args[i],typeof(args));
-    }
-return sum;
 
-// return arguments;
 
+arr1=[45,54,46];
+// arr2=arr1;//will show 30 value pusehd after pushing it  
+arr2=[...arr1];//will not show 30 value pusehd after pushing it  
+arr1.push(30);
+
+console.log(arr2);
+console.log(arr1);
+
+
+// concartination
+arr4=[10,20,30];
+arr5=[40,50];
+arr6=[...arr4,...arr5];
+console.log(arr6);
+// console.log(sum('fdf','dfd',...args1));
+
+
+obj1={
+    name:'zishan'
 }
 
-args1=[45,54,46];
-// console.log(...args1);
-// console.log(args1);
-// arr=args1;
-console.log(sum('fdf','dfd',...args1));
-// console.log(sum('zishan','',...args1));
-// console.log(sum('ya','btech',54,57,43));
+obj2={
+    course:'btech',
+    age:21
+}
 
+obj3={...obj1,...obj2};
+console.log(obj3);
