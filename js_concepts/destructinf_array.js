e2f5695048be4f3f4ad69d15de8d40bf1d29945a
zerry101 <macone394@gmail.com>
2023-05-09 09:56:55 +0530

    m

--- js_concepts/destructinf_array.js
+++ js_concepts/destructinf_array.js
@@ -4,3 +4,15 @@ let [name,...args]=user;
 
 // console.log(age);
 console.log(args);
+
+
+let user1=["zishan",20,"ahmedabad"];
+
+
+function userfn([name,age,city]){
+  return [name,age,city];
+}
+
+let result=userfn(user1);
+
+console.log(result);
