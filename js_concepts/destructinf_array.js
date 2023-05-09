e301b61af2aedb88e431406b3fd36e9672195225
zerry101 <macone394@gmail.com>
2023-05-09 06:43:45 +0530

    m

--- js_concepts/destructinf_array.js
+++ js_concepts/destructinf_array.js
@@ -1,5 +1,6 @@
-let user=["yahoo baba",20,"Delhi"];
+let user=["yahoo baba",20,"Delhi",["male",25000]];
 
-let [name,age=90,city]=user;
+let [name,...args]=user;
 
-console.log(age);
+// console.log(age);
+console.log(args);
