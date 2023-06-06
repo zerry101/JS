6c4d18c3c32dbd75882f8a768b8217599a68185f
zerry101 <macone394@gmail.com>
2023-06-06 08:14:58 +0530

    added ps2 file

---
+++ JS/condirionals.js
@@ -0,0 +1,8 @@
+// let a=prompt("Hey whats your age?");
+
+q="34rtrt"
+x=Number.parseInt(q);
+
+
+console.log(x);
+console.log(typeof(x));
---
+++ JS/ps(2).js
@@ -0,0 +1,2 @@
+x=30;
+x%3==0 || x%2==0?console.log("it is divisible  "):console.log("it is not divisible ");
