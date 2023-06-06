0e0df59288c338bea7e9b176e2e220b2e4518fc3
zerry101 <macone394@gmail.com>
2023-06-06 07:40:12 +0530

    added primitive data types and objects files

---
+++ JS/pr(1).js
@@ -0,0 +1,27 @@
+
+//Q1
+let x = "string";
+
+x += 1;
+console.log(x);
+
+
+// Q2   
+console.log(typeof (x));
+
+// Q3
+const obj = {
+    name: "harry"
+};
+
+// obj=23
+;
+
+// we cannot change const variable to another type once declared but we can change the const object whic will be defined in the memory then we can manipulate object elemnts but not the variable type 
+obj.marks=56;
+console.log(obj);
+console.log(obj.marks);
+console.log(obj['marks']);
+
+// Q5
+
