10c6d9386fc3eaae0f537ea1c5a38a0b655f4780
zerry101 <macone394@gmail.com>
2023-06-12 13:51:50 +0530

    added more chappters and files

---
+++ JS/hoisting.js
@@ -0,0 +1,14 @@
+console.log(a);
+console.log(t);
+// console.log(b);
+
+greet()
+function greet(){
+    console.log('hii hello there ');
+}
+var a=1;
+const  t=1;
+console.log(a);
+// let b=1;
+
+// hoist dont works with arrow functions , let ,const
